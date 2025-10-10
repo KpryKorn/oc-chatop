1121f69503f4d6dd33114451cce4573af6892705
KpryKorn <roffinisacha@gmail.com>
2025-10-10 10:55:58 +0200

    :sparkles: feat(rental): modify getAllRentals method to return a mapped response structure

--- src/main/java/com/projet3/projet3/controller/RentalController.java
+++ src/main/java/com/projet3/projet3/controller/RentalController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/rentals")
@@ -25,9 +26,11 @@ public class RentalController {
     }
 
     @GetMapping
-    public ResponseEntity<List<RentalResponseDTO>> getAllRentals() {
+    public ResponseEntity<Map<String, List<RentalResponseDTO>>> getAllRentals() {
         List<RentalResponseDTO> rentals = rentalService.getAllRentals();
-        return ResponseEntity.ok(rentals);
+
+        Map<String, List<RentalResponseDTO>> mappedRentals = Map.of("rentals", rentals);
+        return ResponseEntity.ok(mappedRentals);
     }
 
     @GetMapping("/{id}")
